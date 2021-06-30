72e4cb9aa5b45109b1ff071e8247337532e87c5f
agustoth56 <56087100+agustoth56@users.noreply.github.com>
2021-07-01 04:01:31 +1200

    Add files via upload

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,11 @@
+//informacion
+var fullName = "Agustin Toth Moreira";
+var career = "IT Technician";
+var dreamJob = "Web Developer";
+
+//cadena mediante template literals
+var stringTempLit = `Hola, me llamo ${fullName} y me dedico a ${career}.
+Estoy cursando este Máster porque me gustaría trabajar en ${dreamJob}`;
+
+//Muestra en pantalla de la variable anterior
+document.write(stringTempLit);
