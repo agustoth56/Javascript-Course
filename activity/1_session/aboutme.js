697e2bd8c2dde8cb6a74c7bb6dcb3c9cf357b064
agustoth56 <56087100+agustoth56@users.noreply.github.com>
2021-07-01 04:38:55 +1200

    2

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -9,4 +9,11 @@ var stringTempLit = `Hola, me llamo ${fullName} y me dedico a ${career}.
 Estoy cursando este Máster porque me gustaría trabajar en ${dreamJob}`;
 
 //Muestra en pantalla de la variable anterior
+var div = document.createElement("div");
+div.id = "ej1";
+div.style = "margin: 10%";
+document.body.appendChild(div);
+var text = document.createTextNode(stringTempLit);
+document.getElementById('ej1').appendChild(text);
+
 console.log(stringTempLit);
