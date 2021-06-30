34b9a0a06f44c7fa2ec6e63405a143b47d53bfb0
agustoth56 <56087100+agustoth56@users.noreply.github.com>
2021-07-01 04:27:45 +1200

    1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -4,8 +4,9 @@ var career = "IT Technician";
 var dreamJob = "Web Developer";
 
 //cadena mediante template literals
+
 var stringTempLit = `Hola, me llamo ${fullName} y me dedico a ${career}.
 Estoy cursando este Máster porque me gustaría trabajar en ${dreamJob}`;
 
 //Muestra en pantalla de la variable anterior
-document.write(stringTempLit);
+console.log(stringTempLit);
