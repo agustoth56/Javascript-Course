dc24707ade42fe3f5d7d6e6a111bed8ef476ffeb
agustoth56 <56087100+agustoth56@users.noreply.github.com>
2021-07-02 15:15:16 +1200

    2

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -11,9 +11,8 @@ Estoy cursando este Máster porque me gustaría trabajar en ${dreamJob}`;
 //Muestra en pantalla de la variable anterior
 var div = document.createElement("div");
 div.id = "ej1";
-div.style = "margin: 10%";
+div.style = "text-align: center; margin: 10%; font-size: 1.2em; padding: 15px; background-color: black; color: white;";
 document.body.appendChild(div);
 var text = document.createTextNode(stringTempLit);
 document.getElementById('ej1').appendChild(text);
-
-console.log(stringTempLit);
+console.log("Ejercicio 1: Se ha mostrado en pantalla");
